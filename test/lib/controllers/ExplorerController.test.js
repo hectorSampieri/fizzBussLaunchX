1c11710833264c1d7a02f55424723e0a20ff13d3
hectorSampieri <hoscmx@outlook.com>
2022-04-27 12:36:30 -0500

    Agregando pruebas del controllador ExplorerController

---
+++ test/lib/controllers/ExplorerController.test.js
@@ -0,0 +1,18 @@
+const ExplorerController = require("../../../lib/controllers/ExplorerController")
+
+
+describe("Pruebas de la clase ExplorerService", () => {
+    test("1) Comprobar metodo getExplorersByMission", () => {
+        const explorersByMission =ExplorerController.getExplorersByMission("node");
+        expect(explorersByMission.length).toBe(10);
+    });
+
+    test("2) Comprobar metodo getExplorersUsernamesByMission", () => {
+        const explorersUsernamesByMission =ExplorerController.getExplorersUsernamesByMission( "node");
+        expect(explorersUsernamesByMission[0]).toBe("ajolonauta1");
+    });
+    test("3) Comprobar metodo getExplorersAmonutByMission", () => {
+        const amountOfExplorersByMission =ExplorerController.getExplorersAmonutByMission( "node");
+        expect(amountOfExplorersByMission).toBe(10);
+    });
+});
