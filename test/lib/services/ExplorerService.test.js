2fe7fac9efbf7ccf67fb47abe3891b7725507cd5
hectorSampieri <hoscmx@outlook.com>
2022-04-27 11:15:26 -0500

    Se completan las pruebas de la clase ExplorerService

--- test/lib/services/ExplorerService.test.js
+++ test/lib/services/ExplorerService.test.js
@@ -5,7 +5,17 @@ describe("Pruebas de la clase ExplorerService", () => {
     test('1) Comprobar metodo filterByMission', () => {
         const explorers =Reader.readJsonFile('./data/explorers.json')
         const explorersByMission =ExplorerService.filterByMission(explorers, 'node')
-        console.log(explorersByMission.length)
         expect(explorersByMission.length).toBe(10)
+    })
+
+    test('2) Comprobar metodo getAmountOfExplorersByMission', () => {
+        const explorers =Reader.readJsonFile('./data/explorers.json')
+        const amountOfExplorersByMission =ExplorerService.getAmountOfExplorersByMission(explorers, 'node')
+        expect(amountOfExplorersByMission).toBe(10)
+    })
+    test('3) Comprobar metodo getExplorersUsernamesByMission', () => {
+        const explorers =Reader.readJsonFile('./data/explorers.json')
+        const explorersUsernamesByMission =ExplorerService.getExplorersUsernamesByMission(explorers, 'node')
+        expect(explorersUsernamesByMission[0]).toBe('ajolonauta1')
     })
     })
