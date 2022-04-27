a28cadac9aca8578bd31768b56bc6c4c571983df
hectorSampieri <hoscmx@outlook.com>
2022-04-27 12:37:49 -0500

    Archivos corregidos por Linter

--- test/lib/controllers/ExplorerController.test.js
+++ test/lib/controllers/ExplorerController.test.js
@@ -1,4 +1,4 @@
-const ExplorerController = require("../../../lib/controllers/ExplorerController")
+const ExplorerController = require("../../../lib/controllers/ExplorerController");
 
 
 describe("Pruebas de la clase ExplorerService", () => {
