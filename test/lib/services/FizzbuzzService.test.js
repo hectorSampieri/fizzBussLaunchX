4de91d56ae2c7c45b4204a060e88bebdab0a90c0
hectorSampieri <hoscmx@outlook.com>
2022-04-27 11:57:17 -0500

    Se agrega la clase FizzBuzzService con las pruebas de unidad

---
+++ test/lib/services/FizzbuzzService.test.js
@@ -0,0 +1,28 @@
+const FizzbuzzService = require('../../../lib/services/FizzbuzzService')
+
+describe("Pruebas de la clase FizzbuzzService", () => {
+    test('1) Comprobar  Score 1', () => {
+        const explorer1= {name: "Explorer1", score: 1}
+        const explorerTest = FizzbuzzService.applyValidationInExplorer(explorer1)
+        console.log(explorerTest)
+         expect(explorerTest.trick).toBe(1)
+    })
+    test('2) Comprobar  Score 3', () => {
+        const explorer1= {name: "Explorer1", score: 3}
+        const explorerTest = FizzbuzzService.applyValidationInExplorer(explorer1)
+        console.log(explorerTest)
+         expect(explorerTest.trick).toBe("FIZZ")
+    })
+    test('3) Comprobar  Score 5', () => {
+        const explorer1= {name: "Explorer1", score: 5}
+        const explorerTest = FizzbuzzService.applyValidationInExplorer(explorer1)
+        console.log(explorerTest)
+         expect(explorerTest.trick).toBe("BUZZ")
+    })
+    test('4) Comprobar  Score 15', () => {
+        const explorer1= {name: "Explorer1", score: 15}
+        const explorerTest = FizzbuzzService.applyValidationInExplorer(explorer1)
+        console.log(explorerTest)
+         expect(explorerTest.trick).toBe("FIZZBUZZ")
+    })
+    })
