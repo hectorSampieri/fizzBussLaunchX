3737b488a64bba21b039d8484e1869dd011ff2ed
hectorSampieri <hoscmx@outlook.com>
2022-04-27 10:59:38 -0500

    Se agrega la clase ExplorerService

--- app.js
+++ app.js
@@ -1,24 +1,3 @@
-const fs = require("fs");
-
-// Part 1 Read json file ===========================
-const rawdata = fs.readFileSync("explorers.json");
-const explorers = JSON.parse(rawdata);
-
-// Part 2: Get the quantity of explorers names in node
-const explorersInNode = explorers.filter((explorer) => explorer.mission == "node");
-//console.log(explorersInNode.length)
-
-// Part4: Get the explorer's usernames in Node
-const explorersInNodeToGetUsernames = explorers.filter((explorer) => explorer.mission == "node");
-const usernamesInNode = explorersInNodeToGetUsernames.map((explorer) => explorer.githubUsername);
-//console.log(usernamesInNode)
-
-// DEAD CODE: Part 5,6,7, please remove this and go to Part 8!
-
-// Part 5: Get a new list of explorers in node, if the score numbers is divisible by 3, I need a new propery called trick, and the value assigned is FIZZ, if not the value should be the score itself.
-// Score: 3, Trick: FIZZ.
-// Score: 4, Trick: 4.
-// Score: 5, Trick: 5.
 
 const assignFizzTrick = function(explorer){
     if(explorer.score%3 === 0){
